d3aa46b0ee68391f112a1c941550534b735e1b8a
Peter Carrero <peter.carrero@gmail.com>
2017-02-15 17:04:08 -0600

    Add messages and ssh to utility container

--- containers/utility/docker-entrypoint.sh
+++ containers/utility/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 if [ -f /utility/init.sql ]; then
+  echo "Creating databases that do not yet exit"
   mysql -h db -u $mariadb_USERNAME -p$mariadb_ROOT_PASSWORD < /utility/init.sql
+  echo "Done."
 fi
+
+echo "Starting ssh daemon"
+/usr/sbin/sshd -D
