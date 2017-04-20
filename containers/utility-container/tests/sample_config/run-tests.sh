90c56affb7f65e28be29bac56ea0bc2196f42a42
nicxvan <nic@nlightened.net>
2017-04-20 15:44:02 -0400

    adding coverage reports

--- containers/utility-container/tests/sample_config/run-tests.sh
+++ containers/utility-container/tests/sample_config/run-tests.sh
@@ -7,7 +7,7 @@ BASE_PATH=/var/www/drupal/web
 BASE_URL=http://nginx:80
 
 mkdir -p /var/www/drupal/tests/coverage
-COVERAGE_ARGS=(--configuration /var/www/drupal/tests/config/phpunit.xml.dist --coverage-clover /var/www/drupal/tests/coverage-report/clover.xml --log-junit /var/www/drupal/tests/coverage-report/junit.xml)
+COVERAGE_ARGS=(--configuration /var/www/drupal/tests/config/phpunit.xml.dist --coverage-clover /var/www/drupal/tests/coverage-report/clover.xml --log-junit /var/www/drupal/tests/coverage-report/junit.xml --coverage-php /var/www/drupal/tests/coverage/phpunit.cov)
 
 BROWSERTEST_OUTPUT_FILE=/var/www/drupal/tests/coverage-report/browser-output.html SIMPLETEST_DB=$TEST_DB SIMPLETEST_BASE_URL=$BASE_URL /var/www/drupal/vendor/bin/phpunit ${COVERAGE_ARGS[@]}
 
